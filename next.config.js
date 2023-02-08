b302670e4d4cd5428834ae9648dc57a6e8d1f6e0
SandovaI <86376236+SandovaI@users.noreply.github.com>
2023-02-07 22:23:39 -0800

    revert

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  resolve: {
-    fallback: {
-      fs: false,
-    },
-  },
 };
 module.exports = nextConfig;

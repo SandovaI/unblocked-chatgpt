06935078f5e9dd3ab8c219993b82469ae8ad18a2
SandovaI <86376236+SandovaI@users.noreply.github.com>
2023-02-15 22:59:20 -0800

    added conversation in chat page

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,29 +1,60 @@
 import { useState } from "react";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/chat.module.css";
+import url from "../constants/api.constants";
 export default function Home() {
   const [answer, setAnswer] = useState("");
   const [error, setError] = useState(null);
   const [status, setStatus] = useState("typing");
-  function handleTextareaChange(e: any) {
-    setAnswer(e.target.value);
-  }
+  const [messages, setMessages]: any = useState([]);
 
   async function handleSubmit(e: any) {
     e.preventDefault();
     setStatus("submitting");
-
     try {
+      const res = await submitForm(answer);
+      setStatus(res);
+      setMessages([
+        ...messages,
+        { answer: answer, bot: false },
+        { answer: res, bot: true },
+      ]);
     } catch (err: any) {
       setStatus("typing");
       setError(err.message);
     }
   }
+
+  function handleTextareaChange(e: any) {
+    setAnswer(e.target.value);
+  }
+  async function submitForm(answer: string) {
+    const res = await fetch(`${url.API_URL}/api/conversation`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ q: answer }),
+      method: "POST",
+    });
+    const message = await res.json();
+    if (!res.ok) {
+      throw new Error(message.error);
+    }
+    return message;
+  }
+
   return (
     <>
       <header>
         <nav></nav>
       </header>
       <main className={styles.main}>
+        <div className={styles.convo}>
+          {messages.map((i: any) => (
+            <p key={i} className={`${i.bot === true && styles.bot}`}>
+              {i.answer}
+            </p>
+          ))}
+        </div>
         <form className={styles.form} onSubmit={handleSubmit}>
           <p>Ask Chat:</p>
           <textarea
@@ -41,7 +72,4 @@ export default function Home() {
       <footer></footer>
     </>
   );
-}
-function setAnswer(value: any) {
-  throw new Error("Function not implemented.");
 }
